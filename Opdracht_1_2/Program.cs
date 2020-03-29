3994554c101ca2fec484ecec109c564cef15b436
Makkele13 <info@markwaarde.nl>
2020-03-30 00:04:52 +0200

    Opdracht 1.2 added

---
+++ Opdracht_1_2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Opdracht_1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("****************************");
+            Console.WriteLine("*** \t Mark Jansen \t ***");
+            Console.WriteLine("****************************");
+
+            // wait for input
+            Console.ReadKey();
+        }
+    }
+}
--- Project_1_1/Program.cs
+++ Project_1_1/Program.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 
-namespace Project_1_1
+namespace Opdracht_1_1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Mark Jansen");
+            Console.WriteLine("Burg. M. van Veenlaan 71");
+            Console.WriteLine("7543 AH \tENSCHEDE");
+            Console.WriteLine("45");
+
+
+            // wait for input
             Console.ReadKey();
         }
+
     }
 }
