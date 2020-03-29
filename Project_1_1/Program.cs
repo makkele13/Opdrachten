20c500bac9d6443b0ab188fd324f89f2929c15b5
Makkele13 <info@markwaarde.nl>
2020-03-29 22:44:41 +0200

    Add project files.

---
+++ Project_1_1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Project_1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
